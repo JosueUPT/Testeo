ee983684afae28c7e03a267bc09ade08a1f9db1b
JosueUPT <jaechambilla@upt.pe>
2024-09-20 03:31:43 -0500

    v1

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '346da30926eae406bf8205f6412dcccfe67fbc0f',
+        'reference' => '3893f2eb7875436645afdeae843f404369ee9d2b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '346da30926eae406bf8205f6412dcccfe67fbc0f',
+            'reference' => '3893f2eb7875436645afdeae843f404369ee9d2b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
